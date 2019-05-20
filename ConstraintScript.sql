3efd2bdb05343f897f6ea454bf34458a29a84eac
Simon van Noppen <simon@vannoppen.com>
2019-05-20 14:55:23 +0200

    Test and Constaint fixed
    Tests en constaint zelf gemaakt

--- ConstraintScript.sql
+++ ConstraintScript.sql
@@ -9,3 +9,10 @@
 |	Versie:		1.0					|
 |	Gemaakt op:	5/7/2019 13:42				|
 \*-------------------------------------------------------------*/
+
+/*===== Constraint 5 AnimalGender =====*/
+/* column ANIMAL(Gender)can be male, female or other.*/
+alter table animal drop constraint if exists CHK_ANIMAL_GENDER;
+
+alter table animal add constraint CHK_ANIMAL_GENDER
+check(gender_s in ('male','female','other'));
--- ConstraintTestScript.sql
+++ ConstraintTestScript.sql
@@ -9,3 +9,92 @@
 |	Versie:		1.0					|
 |	Gemaakt op:	5/7/2019 13:42				|
 \*-------------------------------------------------------------*/
+
+/*===== Constraint 5 AnimalGender =====*/
+
+/* Test should pass when inserting or updating animal gender to 'male' */
+-- insert
+begin transaction;
+insert into species values
+('Monkey','Things with long arms',null,null,null);
+insert into animal values
+(1,'male','Abu' ,'Engeland', '12-12-18', 'Monkey'),
+(2,'male','Koko','Engeland', '10-10-18', 'Monkey');
+rollback;
+
+-- update
+begin transaction;
+insert into species values
+('Monkey','Things with long arms',null,null,null);
+insert into animal values
+(1,'female','Abu' ,'Engeland', '12-12-18', 'Monkey'),
+(2,'female','Koko','Engeland', '10-10-18', 'Monkey');
+update animal
+set gender_s = 'male';
+rollback;
+
+/* Test should pass when inserting or updating animal gender to 'female' */
+-- insert
+-- insert
+begin transaction;
+insert into species values
+('Monkey','Things with long arms',null,null,null);
+insert into animal values
+(1,'female','Abu' ,'Engeland', '12-12-18', 'Monkey'),
+(2,'female','Koko','Engeland', '10-10-18', 'Monkey');
+rollback;
+
+-- update
+begin transaction;
+insert into species values
+('Monkey','Things with long arms',null,null,null);
+insert into animal values
+(1,'male','Abu' ,'Engeland', '12-12-18', 'Monkey'),
+(2,'male','Koko','Engeland', '10-10-18', 'Monkey');
+update animal
+set gender_s = 'female';
+rollback;
+
+/* Test should pass when inserting or updating animal gender to 'other' */
+-- insert
+begin transaction;
+insert into species values
+('Monkey','Things with long arms',null,null,null);
+insert into animal values
+(1,'other','Abu' ,'Engeland', '12-12-18', 'Monkey'),
+(2,'other','Koko','Engeland', '10-10-18', 'Monkey');
+rollback;
+
+-- update
+begin transaction;
+insert into species values
+('Monkey','Things with long arms',null,null,null);
+insert into animal values
+(1,'male','Abu' ,'Engeland', '12-12-18', 'Monkey'),
+(2,'male','Koko','Engeland', '10-10-18', 'Monkey');
+update animal
+set gender_s = 'other';
+rollback;
+
+/* Test should fail when inserting or updating animal gender to something other than 'male', 'female', 'other' */
+-- insert
+begin transaction;
+insert into species values
+('Monkey','Things with long arms',null,null,null);
+insert into animal values
+(1,'male','Abu' ,'Engeland', '12-12-18', 'Monkey'),
+(2,'something','Koko','Engeland', '10-10-18', 'Monkey');
+rollback;
+
+-- update
+begin transaction;
+insert into species values
+('Monkey','Things with long arms',null,null,null);
+insert into animal values
+(1,'male','Abu' ,'Engeland', '12-12-18', 'Monkey'),
+(2,'male','Koko','Engeland', '10-10-18', 'Monkey');
+update animal
+set gender_s = 'something';
+rollback;
+
+/*============*/
