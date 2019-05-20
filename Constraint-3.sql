a98112c86e10f49d916d66125d8365e91f8c4b53
Cumali Karakoc <C.Karakoc@student.han.nl>
2019-05-20 11:35:58 +0200

    Add tests for constraint 3

--- Constraint-3.sql
+++ Constraint-3.sql
@@ -2,3 +2,51 @@ ALTER TABLE "ORDER" DROP CONSTRAINT IF EXISTS CHCK_PAID_HAS_INVOICE;
 
 ALTER TABLE "ORDER" ADD CONSTRAINT CHCK_PAID_HAS_INVOICE
 CHECK(state = 'paid' OR invoice_id IS NULL);
+
+/* test should pass upon inserting a paid order or updating an order state to paid.*/
+-- insert
+BEGIN TRANSACTION;
+INSERT INTO invoice VALUES('1');
+INSERT INTO supplier VALUES('jumbo', '123213', 'ijssellaan');
+INSERT INTO "ORDER" VALUES(1, 'jumbo', 'paid', '2019-12-12', '1');
+ROLLBACK;
+
+-- update 
+BEGIN TRANSACTION;
+INSERT INTO invoice VALUES('1');
+INSERT INTO supplier VALUES('jumbo', '123213', 'ijssellaan');
+INSERT INTO "ORDER" VALUES(1, 'jumbo', 'piad', '2019-12-12', '1');
+UPDATE "ORDER" SET state = 'paid', invoice_id = '1';
+ROLLBACK;
+
+
+/* Tests should raise a check constraint error*/
+--insert
+BEGIN TRANSACTION;
+INSERT INTO invoice VALUES('1');
+INSERT INTO supplier VALUES('jumbo', '123213', 'ijssellaan');
+INSERT INTO "ORDER" VALUES(1, 'jumbo', 'paid', '2019-12-12', null);
+ROLLBACK;
+
+--update 
+BEGIN TRANSACTION;
+INSERT INTO invoice VALUES('1');
+INSERT INTO supplier VALUES('jumbo', '123213', 'ijssellaan');
+INSERT INTO "ORDER" VALUES(1, 'jumbo', 'placed', '2019-12-12', null);
+UPDATE "ORDER" SET state = 'paid', invoice_id = '1';
+ROLLBACK;
+
+/* Tests should not pass if state is not paid and an invoice is attached to the order. */
+BEGIN TRANSACTION;
+INSERT INTO invoice VALUES('1');
+INSERT INTO supplier VALUES('jumbo', '123213', 'ijssellaan');
+INSERT INTO "ORDER" VALUES(1, 'jumbo', 'placed', '2019-12-12', 1);
+ROLLBACK;
+
+--update 
+BEGIN TRANSACTION;
+INSERT INTO invoice VALUES('1');
+INSERT INTO supplier VALUES('jumbo', '123213', 'ijssellaan');
+INSERT INTO "ORDER" VALUES(1, 'jumbo', 'placed', '2019-12-12', 1);
+UPDATE "ORDER" SET state = 'awaiting payment', invoice_id = '1';
+ROLLBACK;
