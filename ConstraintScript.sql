733456b33afd283702f0fae8d77acb011bb1404f
Cumali Karakoc <C.Karakoc@student.han.nl>
2019-05-21 10:30:38 +0200

    Add main scenario tests for the trigger on reintroduction

--- ConstraintScript.sql
+++ ConstraintScript.sql
@@ -10,9 +10,11 @@
 |	Gemaakt op:	5/7/2019 13:42				|
 \*-------------------------------------------------------------*/
 /*===== CONSTRAINT 10 ===== */
+--Een dier kan niet gezien zijn in het wild voor zijn eerste vrijlating.
+-- table SPOTTED
 CREATE OR REPLACE FUNCTION TRP_SPOTTED_AFTER_RELEASE() RETURNS TRIGGER AS $$
    BEGIN
-   IF(NEW.spot_date < (SELECT reintroduction_date FROM REINTRODUCTION where animal_id = new.animal_id ORDER BY reintroduction_date ASC LIMIT 1)) THEN
+   IF(NEW.spot_date < (SELECT reintroduction_date FROM REINTRODUCTION WHERE animal_id = NEW.animal_id ORDER BY reintroduction_date ASC LIMIT 1)) THEN
       RAISE EXCEPTION 'Spot_date must be after the date when the animal is reintroduced to wild.';
 	  END IF;
       RETURN NEW;
@@ -22,4 +24,23 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER TR_SPOTTED_AFTER_RELEASE AFTER INSERT OR UPDATE ON SPOTTED
 FOR EACH ROW 
 EXECUTE PROCEDURE TRP_SPOTTED_AFTER_RELEASE();
+
+--table REINTRODUCTION
+CREATE OR REPLACE FUNCTION TRP_REINTRODUCTION_BEFORE_SPOTTED() RETURNS TRIGGER AS $$
+   BEGIN
+   IF((TG_OP = 'INSERT' OR TG_OP = 'UPDATE') 
+	  AND NEW.reintroduction_date > (SELECT MAX(spot_date) FROM SPOTTED WHERE animal_id = NEW.animal_id)) THEN
+      RAISE EXCEPTION 'Reintroduction date must be before the date when the animal is spotted.';
+	  RETURN NEW;
+	ELSIF(TG_OP = 'DELETE' AND COALESCE((SELECT MIN(spot_date) FROM SPOTTED WHERE animal_id = OLD.animal_id), CURRENT_DATE) < 
+		  						COALESCE((SELECT MIN(reintroduction_date) FROM REINTRODUCTION WHERE animal_id = OLD.animal_id), CURRENT_DATE)) THEN
+		RAISE EXCEPTION 'The reintoroduction date % of animal % may not be deleted because this animal is spotted after the reintroduction. Remove the associated spot_date first please.', OLD.reintroduction_date, OLD.animal_id;
+		END IF;
+      RETURN OLD;
+   END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER TR_REINTRODUCTION_BEFORE_SPOTTED AFTER INSERT OR UPDATE OR DELETE ON REINTRODUCTION
+FOR EACH ROW 
+EXECUTE PROCEDURE TRP_REINTRODUCTION_BEFORE_SPOTTED();
 /*==============*/
--- ConstraintTestScript.sql
+++ ConstraintTestScript.sql
@@ -19,4 +19,28 @@ ALTER TABLE spotted DROP CONSTRAINT fk_animal_spotted;
 INSERT INTO reintroduction VALUES('an-1', '2018-12-12', 'location', null);
 INSERT INTO spotted VALUES('an-1', '2018-10-11');
 ROLLBACK;
+
+/*Test should raise an error if there is no reintroduction_date before the spot_date*/
+BEGIN TRANSACTION;
+-- drop fk constraints
+ALTER TABLE reintroduction DROP CONSTRAINT fk_animal_reintroduction;
+ALTER TABLE spotted DROP CONSTRAINT fk_animal_spotted;
+
+INSERT INTO reintroduction VALUES('an-1', '2018-12-12', 'location', null);
+INSERT INTO spotted VALUES('an-1', '2019-01-01');
+
+UPDATE reintroduction SET reintroduction_date = '2019-05-05';
+ROLLBACK;
+
+--delete
+BEGIN TRANSACTION;
+-- drop fk constraints
+ALTER TABLE reintroduction DROP CONSTRAINT fk_animal_reintroduction;
+ALTER TABLE spotted DROP CONSTRAINT fk_animal_spotted;
+
+INSERT INTO reintroduction VALUES('an-1', '2018-12-12', 'location', null);
+INSERT INTO spotted VALUES('an-1', '2019-01-01');
+
+DELETE FROM reintroduction WHERE animal_id = 'an-1';
+ROLLBACK;
 /*=============*/
