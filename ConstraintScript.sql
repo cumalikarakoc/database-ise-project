8fe8b23d7604c2a0dd9ec6257351c3905998d992
Henkie van den Oord <henkie344@gmail.com>
2019-05-21 14:04:13 +0200

    Een van de states veranderd

--- ConstraintScript.sql
+++ ConstraintScript.sql
@@ -13,19 +13,10 @@
 /* Constraint 1 OrderStates
 Column ORDER(State) An order must have one of the following states: Paid, Not complete, Awaiting payment, placed.
 
-=========================================
-= State			= Possible?	=
-=========================================
-= Placed		= Yes		=
-= Awaiting payment 	= Yes		=
-= Delivered		= No		=
-= Not paid		= No		=
-=========================================
-
 To apply this constraint, a check constraint needs to be created. 
 The constraint checks if an order has one of the four states mentioned before.
 */
 
 alter table "ORDER"
 add constraint CHK_ORDER_STATE 
-check (State in('Order completed', 'Paid', 'Awaiting payment', 'Not complete'));
+check (State in('Placed', 'Paid', 'Awaiting payment', 'Not complete'));
