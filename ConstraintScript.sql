df8dfbe6b5ce51ec59d808497cc098ee33140406
Rico Salemon <ricosalemon@gmail.com>
2019-05-23 11:21:09 +0200

    Revert "Merge pull request #19 from cumalikarakoc/feature/constraint-20"
    This reverts commit 0c59d18cd97e3916859b668fe8fc7ee9a4a0e342.

--- ConstraintScript.sql
+++ ConstraintScript.sql
@@ -214,20 +214,12 @@ CREATE TRIGGER TR_DISCREPANCY_DATE
   ON discrepancy
   FOR EACH ROW
   EXECUTE PROCEDURE TR_DISCREPANCY_DATE_FUNC();
-
 /*===== Constraint 17 StockAmount ======*/
 /* column STOCK(Amount) Amount must be higher or equal to 0. */
 ALTER TABLE "stock" DROP CONSTRAINT IF EXISTS CHK_STOCK_AMOUNT;
 
 ALTER TABLE "stock" ADD CONSTRAINT CHK_STOCK_AMOUNT  
 CHECK (amount >= 0);
-
-/*===== Constraint 20 MaturityAge ======*/
-/* column SPECIES_GENDER(Maturity_age) Age must be higher or equal to 0. */
-ALTER TABLE "species_gender" DROP CONSTRAINT IF EXISTS CHK_MATURITY_AGE ;
-
-ALTER TABLE "species_gender" ADD CONSTRAINT CHK_MATURITY_AGE   
-CHECK (maturity_age >= 0);
 
 
 /*===== CONSTRAINT 21 SpeciesWeight =====*/
