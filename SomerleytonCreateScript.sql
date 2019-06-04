ee13e45e1500e5e2b1a3430d6c77808ad33f39d2
jeroen <jerrik1998@gmail.com>
2019-06-04 15:40:43 +0200

    Created history trigger

--- SomerleytonCreateScript.sql
+++ SomerleytonCreateScript.sql
@@ -9,6 +9,30 @@
 |	Versie:		1.0					|
 |	Gemaakt op:	17-5-2019 10:48:23			|
 \*-------------------------------------------------------------*/
+/*trigger for adding records to history history*/
+CREATE FUNCTION tgr_change_history_trigger() RETURNS trigger AS $$
+DECLARE
+    CurrentUser text = (select current_user);
+BEGIN
+    IF      TG_OP = 'INSERT'
+    THEN
+        EXECUTE 'INSERT INTO hist_'|| quote_ident(TG_TABLE_NAME) ||' (operation, Who, new_val)
+        VALUES ('|| quote_literal(TG_OP) ||', ' || quote_literal(CurrentUser) ||', ' || quote_literal(row_to_json(NEW)) ||');';
+        RETURN NEW;
+    ELSIF   TG_OP = 'UPDATE'
+    THEN
+        EXECUTE 'INSERT INTO hist_'|| quote_ident(TG_TABLE_NAME) ||' (operation, Who, new_val, old_val)
+        VALUES ('|| quote_literal(TG_OP) || ', '|| quote_literal(CurrentUser) ||', "'|| quote_literal(row_to_json(NEW)) || '", '|| quote_literal(row_to_json(OLD)) ||');';
+        RETURN NEW;
+    ELSIF   TG_OP = 'DELETE'
+    THEN
+        EXECUTE 'INSERT INTO hist_'|| quote_ident(TG_TABLE_NAME) ||'(TableName, operation, Who, old_val)
+        VALUES ('|| quote_literal(TG_RELNAME) ||', '|| quote_literal(TG_OP) || ', '|| quote_literal(CurrentUser) ||', '|| quote_literal(row_to_json(OLD)) ||');';
+        RETURN OLD;
+    END IF;
+END;
+$$ LANGUAGE 'plpgsql';
+
 /* Event trigger for creating a history table for each tables created*/
 create or replace function trg_create_table_func()
   returns event_trigger
@@ -29,6 +53,10 @@ begin
       end if;
       -- remove public. (schema name) from table identification to get table name
       name_of = replace(name_of, 'public.', '');
+
+      EXECUTE 'CREATE TRIGGER History_trigger BEFORE INSERT OR UPDATE OR DELETE ON '|| quote_ident(name_of) ||'
+                       FOR EACH ROW EXECUTE PROCEDURE tgr_change_history_trigger();' ;
+
       name_of = 'hist_' || name_of;
       execute 'create table if not exists ' || name_of || '( Id serial not null, Tstamp timestamp default now(),
                  Operation    text not null,
